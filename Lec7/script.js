fee121b185aa9ab87d3c2b5cb0da8462dff466a4
hansrajathole <hansrajathole1@gmail.com>
2026-06-18 10:13:44 +0530

    js basics and datatype

---
+++ Lec7/script.js
@@ -0,0 +1,106 @@
+// var x = undefined;
+
+// console.log(x);
+// console.log(y);
+
+
+
+
+
+
+
+
+
+
+// var x = 10;
+
+// let x = 20;
+
+// console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(a);
+// var a
+//  a = 20.5;
+
+// let b = 30;
+// // let b = 40;
+
+// const pi
+// pi = 3.14;
+
+
+// let a = 10;
+// let chacha = "n";
+// let bool = true;
+// let x = undefined;
+// let y = null;
+// console.log(typeof a);
+// console.log(typeof chacha);
+// console.log(typeof bool);
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+// let x = "2";
+// let y = 2;
+
+// console.log(x == y);
+// console.log(x === y);
+
+// let x = Number(prompt("enter value : "))
+
+
+
+// let chacha = "100";
+
+// let x = 5 
+
+// x += parseInt(chacha)
+// console.log(typeof x); //Number
+// console.log( x); // 105 , 5100
+
+
+// let bool = "raj" ;
+
+// let x = parseInt(bool);
+
+// console.log(x);
+// console.log(raj/15);
+
+
+// let a = 10;
+
+// {
+//     let a = 20;
+//     console.log(a);
+    
+// }
+
+// console.log(a);
+
+
+
+var a = 10;
+var b = a;
+b += 40;
+
+console.log(a);
+console.log(b);
+
+
