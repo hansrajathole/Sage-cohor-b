9b14660bada6c1ff08416dfbc17c30474d1cbefe
hansrajathole <hansrajathole1@gmail.com>
2026-06-19 23:22:32 +0530

    array and array methods

---
+++ Lec9/script.js
@@ -0,0 +1,77 @@
+
+
+// let arr = [10,20,30,40]
+// let copyArr = arr
+// copyArr.push(80)
+// console.log(arr);
+// console.log(copyArr);
+
+
+// let arr = [10 , 20, 30, 40, 50];
+// console.log(arr);
+
+// console.log(arr.length);
+// let last = arr.pop()
+// console.log(last);
+// console.log(arr.push(100));
+// arr.shift()
+// arr.unshift(5)
+
+// console.log(arr.slice(0,2));
+// arr.splice(0,3,20,30,40)
+
+// console.log(arr.indexOf("hans"));
+// console.log(arr.includes(10));
+
+
+
+// console.log(arr);
+
+
+
+
+// Map method
+// let arr = [10 , 20, 30, 40, 50];
+// let val = arr.map((num , idx ,aarrr)=>{
+//     console.log(num , " ===> " ,idx);
+//     num + 5
+// })
+
+// let val = arr.map((num , idx ,aarrr)=>num+5)
+
+// let val = arr.map(num=>num+5)
+
+
+// console.log(val);
+
+
+
+
+// Filter 
+// let arr = [1,2,3,4,5,6,7,8,5,9,10,5]
+
+
+// let retrunArr = arr.filter((elem , idx)=>{
+//     if(elem == 5){
+//         console.log(5 ,"===>" ,idx);
+        
+//     }
+//    return elem == 5
+// })
+
+// console.log(retrunArr);
+
+
+
+
+// Reduce
+
+// let arr = [10,20,30,40];
+
+
+// let val = arr.reduce((prev , curr)=>{
+//     console.log("prev ===>",prev ,"curr ===>", curr);
+//     return curr+prev
+// },100)
+
+// console.log(val);
